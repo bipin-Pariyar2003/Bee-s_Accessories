9751690d9d0cd31f84ee3c239d028fbc0b20060c
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-05-31 00:37:26 +0545

    designed login page

---
+++ frontend/src/components/Login/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <dialog id="my_modal_3" className="modal ">
+        <div className="modal-box w-96">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg underline">Log In</h3>
+          <div className="py-6 ">
+            <form className="flex flex-col gap-6 mt-4">
+              <input
+                type="email"
+                placeholder="Email"
+                className="input w-full outline-none"
+                required
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="input input-bordered w-full"
+                required
+              />
+              <div className="flex items-center justify-center gap-4">
+                <button className="btn bg-pink-500 text-white w-20">Log In</button>
+              </div>
+            </form>
+          </div>
+          <p className="text-center text-sm">
+            Don't have an account?{" "}
+            <a href="/register" className="text-pink-500 underline">
+              Register
+            </a>
+          </p>
+        </div>
+      </dialog>
+    </>
+  );
+};
+export default Login;
