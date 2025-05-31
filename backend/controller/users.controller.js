92a0741aab01b5e4e737590c13db821e47845dae
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-05-31 22:20:58 +0545

    backend login logic added

--- backend/controller/users.controller.js
+++ backend/controller/users.controller.js
@@ -21,3 +21,22 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Error fetching users", error });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!user || !isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    } else {
+      res.status(200).json({
+        message: "Login successful",
+        user: { _id: user._id, fullname: user.fullname, email: user.email },
+      });
+    }
+  } catch (error) {
+    console.log("Login error:", error);
+    res.status(500).json({ message: "Error logging in", error });
+  }
+};
--- backend/route/users.route.js
+++ backend/route/users.route.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { signup } from "../controller/users.controller.js";
+import { signup, login } from "../controller/users.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
 export default router;
