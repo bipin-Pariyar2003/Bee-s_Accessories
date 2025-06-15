a9048510c974b6e5135d735204abd71c9f8bbd29
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-06-15 22:07:49 +0545

    browse all added

--- frontend/src/views/Home/index.jsx
+++ frontend/src/views/Home/index.jsx
@@ -3,13 +3,23 @@ import Navbar from "components/Navbar";
 import Banner from "components/Banner";
 import Footer from "components/Footer";
 import Cards from "components/Cards";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar />
       <Banner />
       <Cards />
+      <div className="text-center mt-16">
+        <button
+          className="btn bg-green-700 text-white"
+          onClick={() => navigate("/browse")}
+        >
+          Browse All Products
+        </button>
+      </div>
       <Footer />
     </>
   );
