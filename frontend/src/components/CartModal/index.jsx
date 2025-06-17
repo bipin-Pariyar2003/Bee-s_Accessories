00b3e302517bb35cca514c81552f466da65688e8
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-06-17 19:10:39 +0545

    my cart design

--- frontend/src/components/CartModal/index.jsx
+++ frontend/src/components/CartModal/index.jsx
@@ -6,13 +6,54 @@ const CartModal = () => {
   return (
     <>
       <dialog id="CartModal" className="modal">
-        <div className="modal-box">
-          <div>
-            <h1 className="text-2xl font-bold text-pink-400">
-              You will see your cart here
-            </h1>
+        <div className="modal-box bg-blue-100">
+          <div className="bg-pink-100 p-12 rounded-lg w-full h-full flex-1">
+            <h1 className="underline">Cart Details</h1>
+            <div className="overflow-x-auto">
+              <table className="table">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th>S.N</th>
+                    <th>Particulars</th>
+                    <th>Qty</th>
+                    <th>Price</th>
+                    <th>Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* row 1 */}
+                  <tr>
+                    <th>1</th>
+                    <td>Phone Charms</td>
+                    <td>2</td>
+                    <td>100</td>
+                    <td>200</td>
+                  </tr>
+                  {/* row 2 */}
+                  <tr>
+                    <th>2</th>
+                    <td>Chunky Ring</td>
+                    <td>1</td>
+                    <td>200</td>
+                    <td>200</td>
+                  </tr>
+                  {/* row 3 */}
+                  <tr>
+                    <th>3</th>
+                    <td>Phone Charms</td>
+                    <td>2</td>
+                    <td>100</td>
+                    <td>200</td>
+                  </tr>
+                </tbody>
+              </table>
+              <h1>
+                Total: <span className="text-green-700 font-bold">Rs. 600</span>
+              </h1>
+            </div>
           </div>
-          <div></div>
+
           <div className="modal-action">
             <form method="dialog" className="flex gap-2">
               {/* if there is a button in form, it will close the modal */}
